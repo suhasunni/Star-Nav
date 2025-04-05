1175378f994f967451aa14d4e122dca796bde79b
Suhas Unni <suhasunni6@gmail.com>
2025-04-05 00:07:37 -0400

    clean backend

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from .views.distance import distance
 from .views.bearing import bearing
 from .views.star import star
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('distance', distance, name="distance"),
     path('bearing', bearing, name="bearing"),
     path('star', star, name="star"),
